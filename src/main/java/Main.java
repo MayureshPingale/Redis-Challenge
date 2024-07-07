29749fee8f7e2c9c38f1b64499be583bf17c134e
Mayuresh Pingale <mayureshpingale1156@gmail.com>
2024-07-07 18:51:01 -0500

    Multiple PING Command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     static ServerSocket serverSocket = null;
@@ -26,6 +30,12 @@ public class Main {
           in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
           String input = in.readLine();
+          System.out.println(input);
+
+          out.write("+PONG" + CRLF);
+          out.flush();
+
+          input = in.readLine();
           System.out.println(input);
 
           out.write("+PONG" + CRLF);

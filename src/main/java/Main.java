72f87bbd018864e947f79e836457b1d5dd687abf
Mayuresh Pingale <mayureshpingale1156@gmail.com>
2024-07-08 19:25:09 -0500

    Added Concurrency to the Redis Server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,55 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class Main{
     static ServerSocket serverSocket = null;
-    static Socket clientSocket = null;
     static int port = 6379;
-    static PrintWriter out = null;
-    static BufferedReader in = null;
     static String CRLF = "\r\n";
 
-  public static void main(String[] args){
+
+    static class Client extends Thread {
+      Socket clientSocket;
+      PrintWriter out;
+      BufferedReader in;
+
+      public Client(Socket clientSocket) throws Exception {
+        this.clientSocket = clientSocket;
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+      }
+
+      @Override
+      public void run(){
+        String input;
+        try {
+          while((input = in.readLine()) != null) {
+            System.out.println(input);
+            if(input.equalsIgnoreCase("PING")) {
+              out.write("+PONG" + CRLF);
+              out.flush();
+              Thread.sleep(10);
+            }
+          }
+        } catch (IOException | InterruptedException e) {
+          System.err.println(e);
+        }
+  
+        deleteConnnection();
+      }
+
+      public void deleteConnnection() {
+        
+        try {
+          in.close();
+        } catch (IOException e) {
+          System.err.println(e);
+        }
+
+        out.checkError();
+      }
+    }
+
+  public static void main(String[] args) throws Exception {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
@@ -21,41 +61,15 @@ public class Main {
         try {
           serverSocket = new ServerSocket(port);
           serverSocket.setReuseAddress(true);
-         
+
+          while(true) {
           // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-
-          // Intialize The output and input connections to the socket
-          out = new PrintWriter(clientSocket.getOutputStream(), true);
-          in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-          String input;
-
-          while( (input = in.readLine()) != null) {
-            System.out.println(input);
-            if(input.equalsIgnoreCase("PING")) {
-              out.write("+PONG" + CRLF);
-              out.flush();
-            }
-          } 
-
-        } catch (IOException e) {
+            Socket clientSocket = serverSocket.accept();
+            new Client(clientSocket).start();
+          }
+           
+        } catch (Exception e) {
           System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           stop();
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
-       }
-  }
-
-  public static void stop() throws IOException {
-    if(clientSocket  != null) {
-      clientSocket.close();;
-    }
-    in.close();
-    out.close();
-    serverSocket.close();
+       } 
   }
 }
